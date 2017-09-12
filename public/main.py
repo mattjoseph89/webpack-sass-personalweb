4cb8a9f5ca644933ebf8939e90abea632cb5bd5b
Matt Joseph Wurth <mattjwurth@gmail.com>
2017-09-12 11:04:50 -0500

    first commit

---
+++ public/main.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+def fib(n)=fib(n-1)+fib(n-2)
+
+print fib(15)
+
+
+fib(0)=1
+fib(1)=1 
+fib(2)=2
+fib(3)=3
+fib(4)=5
+fib(5)=8
+fib(6)=13
+fib(7)=21
+fib(8)=35
+fib(9)=56
+fib(10)=91
+
+fib(11)=fib(10)+fib(9)
